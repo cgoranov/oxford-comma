3aab4e7bc19e0335eaa36820e4bcaa464bbd9022
Che <cgoranov@gmail.com>
2021-03-02 00:00:20 -0600

    commit tree de40bf534c8685c1eb0e8a79adaf2276b697bd2e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size >= 3
+        last_word = array.last
+        array.pop
+        array << "and" 
+        return array.join(", ") << " #{last_word}" 
+    else
+        "error"
+    end
 end
+    
